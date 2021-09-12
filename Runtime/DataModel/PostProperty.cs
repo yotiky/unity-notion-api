2be6e4cd2005d169a830cbcdb0842012f30523f7
yotiky <yotiky@gmail.com>
2021-09-12 23:23:54 +0900

    add support for create Page to Database

---
+++ Runtime/DataModel/PostProperty.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BennyKok.NotionAPI.Post
+{
+    [Serializable]
+    public class Parent
+    {
+        public string type;
+        public string database_id;
+    }
+
+    [Serializable]
+    public class OptionEntry
+    {
+        //public string id;
+        public string name;
+        //public string color;
+    }
+
+    [Serializable]
+    public class SelectProperty
+    {
+        //public string id;
+        //public string name;
+        //public string type;
+        public OptionEntry select;
+    }
+
+    [Serializable]
+    public class MultiSelectProperty
+    {
+        //public string id;
+        //public string name;
+        //public string type;
+        public OptionEntry[] multi_select;
+    }
+
+    [Serializable]
+    public class TitleProperty
+    {
+        //public string id;
+        //public string name;
+        //public string type;
+        public Text[] title;
+    }
+
+    [Serializable]
+    public class TextProperty
+    {
+        //public string id;
+        //public string type;
+        public Text[] rich_text;
+    }
+
+    [Serializable]
+    public class Text
+    {
+        //public string type;
+        public TextContent text;
+        public Annotations annotations = new Annotations();
+        //public string plain_text;
+        //public object href;
+
+        [Serializable]
+        public class TextContent
+        {
+            public string content;
+            public object link;
+        }
+
+        [Serializable]
+        public class Annotations
+        {
+            public bool bold;
+            public bool italic;
+            public bool strikethrough;
+            public bool underline;
+            public bool code;
+            public string color = "default";
+        }
+    }
+
+    [Serializable]
+    public class NumberProperty
+    {
+        //public string id;
+        //public string name;
+        //public string type;
+        public float number;
+    }
+
+    [Serializable]
+    public class CheckboxProperty
+    {
+        //public string id;
+        //public string name;
+        //public string type;
+        public bool checkbox;
+    }
+
+    [Serializable]
+    public class DateProperty
+    {
+        //public string id;
+        //public string name;
+        //public string type;
+        public Date date;
+    }
+
+    [Serializable]
+    public class Date
+    {
+        public string start;
+        public string end;
+    }
+
+    [Serializable]
+    public class EmailProperty
+    {
+        //public string id;
+        //public string name;
+        //public string type;
+        public string email;
+    }
+
+    [Serializable]
+    public class PhoneNumberProperty
+    {
+        //public string id;
+        //public string name;
+        //public string type;
+        public string phone_number;
+    }
+
+    [Serializable]
+    public class UrlProperty
+    {
+        //public string id;
+        //public string name;
+        //public string type;
+        public string url;
+    }
+
+    [Serializable]
+    public class PersonProperty
+    {
+        //public string id;
+        //public string name;
+        //public string type;
+        public People[] people;
+    }
+
+    [Serializable]
+    public class People
+    {
+        public string @object;
+        public string id;
+    }
+}
--- Runtime/NotionAPITest.cs
+++ Runtime/NotionAPITest.cs
@@ -34,6 +34,76 @@ namespace BennyKok.NotionAPI
                 Debug.Log(JsonUtility.ToJson(db));
             });
 #endif
+
+            var param = new CardDatabasePostRequest
+            {
+                parent = new Post.Parent { database_id = database_id },
+                properties = new CardDatabasePostProperties
+                {
+                    Name = new Post.TitleProperty
+                    {
+                        title = new[] { new Post.Text { text = new Post.Text.TextContent { content = "Attack1" } } }
+                    },
+                    Tags = new Post.MultiSelectProperty
+                    {
+                        multi_select = new[] { new Post.OptionEntry { name = "SomeTag" } }
+                    },
+                    Description = new Post.TextProperty
+                    {
+                        rich_text = new[] {
+                            new Post.Text { 
+                                text = new Post.Text.TextContent { content = "Sample data.", },
+                                annotations = new Post.Text.Annotations
+                                {
+                                    bold = true, color = "green", code = true, italic = true, strikethrough = true, underline = true,
+                                }
+                        },
+                    }
+                    },
+                    UseTime = new Post.NumberProperty
+                    {
+                        number = 100,
+                    },
+                    Type = new Post.SelectProperty
+                    {
+                        select = new Post.OptionEntry { name = "SomeAttack" },
+                    },
+                    Date = new Post.DateProperty
+                    {
+                        date = new Post.Date { start = DateTime.Now.ToString("yyyy-MM-dd'T'HH:mm:ss'Z'"), end = DateTime.Now.ToString("yyyy-MM-dd'T'HH:mm:ss'Z'") },
+                    },
+                    Person = new Post.PersonProperty
+                    {
+                        people = new[] {
+                        new Post.People { @object = "user", id = "valid id" }
+                    }
+                    },
+                    IsActive = new Post.CheckboxProperty
+                    {
+                        checkbox = true,
+                    },
+                    Url = new Post.UrlProperty
+                    {
+                        url = "https://www.hogehoge.com",
+                    },
+                    Email = new Post.EmailProperty
+                    {
+                        email = "hoge@email.com",
+                    },
+                    Phone = new Post.PhoneNumberProperty
+                    {
+                        phone_number = "012344445555",
+                    },
+                }
+            };
+
+            var json = JsonUtility.ToJson(param);
+            Debug.Log(json);
+
+            yield return api.CreatePage(param, (res) =>
+            {
+                Debug.Log(res);
+            });
         }
 
         [Serializable]
@@ -69,5 +139,28 @@ namespace BennyKok.NotionAPI
 
         [Serializable]
         public class CardDatabasebPage : Page<CardDatabaseProperties> { }
+
+        [Serializable]
+        public class CardDatabasePostRequest
+        {
+            public Post.Parent parent;
+            public CardDatabasePostProperties properties;
+        }
+
+        [Serializable]
+        public class CardDatabasePostProperties
+        {
+            public Post.MultiSelectProperty Tags;
+            public Post.TitleProperty Name;
+            public Post.CheckboxProperty IsActive;
+            public Post.DateProperty Date;
+            public Post.SelectProperty Type;
+            public Post.NumberProperty UseTime;
+            public Post.TextProperty Description;
+            public Post.UrlProperty Url;
+            public Post.EmailProperty Email;
+            public Post.PhoneNumberProperty Phone;
+            public Post.PersonProperty Person;
+        }
     }
 }
